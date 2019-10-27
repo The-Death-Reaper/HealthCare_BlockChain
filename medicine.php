a925f9a0015685407179bf75b71b38ffa20779f7
Mayur Peshve <mayurpeshve2@gmail.com>
2019-10-27 08:34:08 +0530

    Add files via upload

--- medicine.php
+++ medicine.php
@@ -1,12 +1,19 @@
-<? php
-	$patientName = $DoB = $OrganDonor = $Organ = $BloodPressure = $GlucoseLevel = "";
+<?php
+	$patientName = $DoB = $OrganDonor = $Organ = $BloodPressure = $GlucoseLevel = $PhoneNumber = $BloodType = "";
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
-		$patientName = test_input($_POST["patientName"]);
+		$patientName = test_input($_POST["p_name"]);
 		$DoB = test_input($_POST["DoB"]);
 		$OrganDonor = test_input($_POST["OrganDonor"]);
 		$Organ = test_input($_POST["Organ"]);
-		$BloodPressure = test_input($_POST["BloodPressure"]);
-		$GlucoseLevel = test_input($_POST["GlucoseLevel"]);
+		$PhoneNumber = test_input($_POST["number"]);
+		$BloodType = test_input($_POST["b_type"]);
+		//$BloodPressure = test_input($_POST["BloodPressure"]);
+		//$GlucoseLevel = test_input($_POST["GlucoseLevel"]);
+		$file = "NotEncrypted.txt";
+	$current = file_get_contents("NotEncrypted.txt");
+	$txt = $patientName.' '.$DoB.' '.$PhoneNumber.' '.$Organ;
+	$current.=$txt;
+	file_put_contents($file, $txt);
 
 	}
 	function test_input($data){
@@ -15,18 +22,32 @@
 		$data = htmlspecialchars($data);
 		return $data;
 	}
+	$keys = array(45312,2,67328);
+	// $GenerateKeyCommand = escapeshellcmd('python generateKeys.py');
+	// $keys = `python generateKeys.py`;
+	// echo $keys;
 
+	// $keys = explode("|", $keys);
 
-	$GenerateKeyCommand = escapeshellcmd('python3 generateKeys.py');
-	$keys = shell_exec($GenerateKeyCommand);
-	$keys = explode("|", $keys);
+	// $arr = array_keys($keys);
 	$publicKeyE = $keys[1];
 	$publicKeyN = $keys[0];
 	$privateKey = $keys[2];
+	// echo $publicKeyN;
+	// print_r($keys);
 
+	// $EncryptCommand = escapeshellcmd('python EncryptMessage.py $publicKeyN $publicKeyE $patientName $DoB $OrganDonor');
+	// $EncryptMessage = `python EncryptMessage.py $publicKeyN $publicKeyE $patientName $DoB $OrganDonor`;
+	// $EncryptMessage = shell_exec($EncryptCommand);
+	// echo $EncryptMessage;
+	$file = "NotEncrypted.txt";
+	$current = file_get_contents("NotEncrypted.txt");
+	$txt = $patientName.' '.$DoB.' '.$PhoneNumber;
+	$current.=$txt;
+	file_put_contents($file, $txt);
 
-	$EncryptCommand = escapeshellcmd('python3 EncryptMessage.py $publicKeyN $publicKeyE $patientName $DoB $OrganDonor');
-	$EncryptMessage = shell_exec($EncryptCommand);
-	$EncryptMessage = explode("|",$EncryptMessage);
-	
+	header("http://localhost/HashCode/index.html/");
+
+    // echo "HELLO IM A BEAST";
+
 ?>
